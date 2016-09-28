7c34a04d6de8f641e951af3460224c72710bc450
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-28 21:11:58 +0200

    Add project files.

---
+++ TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.UnitTests
+{
+    [TestFixture]
+    public class GameWinnerServiceTests
+    {
+        [Test]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+            var gameBoard = new char[3, 3] {{' ', ' ', ' '},
+                                            {' ', ' ', ' '},
+                                            {' ', ' ', ' '}};
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TicTacToe.UnitTests/gameWinnerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToe.UnitTests
+{
+    internal class gameWinnerService
+    {
+        internal static object Validate(char[,] gameBoard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
